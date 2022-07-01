866cc370f3f9827c611ffd1616a12786d03a3783
airoble-1 <airoble2019@gmail.com>
2022-07-01 12:40:28 -0400

    Add project files.

---
+++ AddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using AddressBook.Models;
+
+namespace AddressBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }    
+    }
+}
